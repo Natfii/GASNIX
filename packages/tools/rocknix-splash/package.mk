0a2e1fd16fd42ad581cba0f3c1b55618769f9a4b
Natali Caggiano <natalicaggiano@outlook.com>
2025-06-02 13:11:19 -0400

    Squashed commit of the following:
    commit 830b9eab94f404238f1264e67ac0df57fdddb055
    Author: natfii <natalicaggiano@outlook.com>
    Date:   Mon Jun 2 13:06:43 2025 -0400
    
        updated .gitignore
    
    commit e7df65b37d0e277b38e89b40811b123a6f670154
    Author: natfii <natalicaggiano@outlook.com>
    Date:   Mon Jun 2 10:33:18 2025 -0400
    
        updated version for splash repo
    
    commit b8e360433e73ec5ffa1376e24642153c9326bf5a
    Author: natfii <natalicaggiano@outlook.com>
    Date:   Sun Jun 1 22:06:24 2025 -0400
    
        start of splash change and scrape fixed
    
    commit 8446fd9d329e37ac737f43f401607dc2fefa3550
    Author: Natali Caggiano <natalicaggiano@outlook.com>
    Date:   Sun Jun 1 17:06:27 2025 -0400
    
        First test splash screen
    
    commit 18e25b7d5eff084ec8b5b51e2a54d3665f66d4ac
    Author: Natali Caggiano <natalicaggiano@outlook.com>
    Date:   Sun Jun 1 13:34:49 2025 -0400
    
        Update package.mk
    
    commit 4f62609bc55d79f2f94ced7bb860be859b85a145
    Author: Natali Caggiano <natalicaggiano@outlook.com>
    Date:   Thu May 29 14:57:32 2025 -0400
    
        Squashed commit of the following:
    
        commit 026bcdb80df0795eb9afa2b44e1aa0983bd1d3d2
        Author: Natfii <natalicaggiano@outlook.com>
        Date:   Thu May 29 13:33:09 2025 -0400
    
            Update README.md
    
        commit 83f4322ffdcaa29d9d877a8e0a92248c5e6d8fac
        Author: Natali Caggiano <natalicaggiano@outlook.com>
        Date:   Thu May 29 13:21:56 2025 -0400
    
            README
    
        commit ccc82615c313be3bd8a2ec14ea19957bf2dcae02
        Author: Natfii <natalicaggiano@outlook.com>
        Date:   Wed May 28 23:03:35 2025 -0400
    
            Update emulationstation2.po
    
        commit 9f47ba88524efdd87a32f408e8e91917b722f494
        Author: Natali Caggiano <natalicaggiano@outlook.com>
        Date:   Wed May 28 22:55:55 2025 -0400
    
            Emulationstation assets
    
        commit 06e3652b61f807bfc06330d4dbfbefdb65d3312d
        Author: Natali Caggiano <natalicaggiano@outlook.com>
        Date:   Wed May 28 12:59:31 2025 -0400
    
            Update README.md
    
        commit 61e00403505a63689649ff84c75a399bad760ed9
        Author: Natali Caggiano <natalicaggiano@outlook.com>
        Date:   Wed May 28 12:57:26 2025 -0400
    
            Cleaned up devices I don't want to work on and updated the README

--- packages/tools/rocknix-splash/package.mk
+++ packages/tools/rocknix-splash/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2025 ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="rocknix-splash"
-PKG_VERSION="1.0.3"
+PKG_VERSION="1.0.1"
 PKG_LICENSE="GPL"
 PKG_SITE="https://rocknix.org"
-PKG_URL="https://github.com/ROCKNIX/${PKG_NAME}/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/Natfii/${PKG_NAME}/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_INIT="toolchain"
 PKG_LONGDESC="ROCKNIX splash screen application"
